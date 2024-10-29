63ea91698c764528ef2eedcf66b04c06a3bdb6d0
EEEeEFA <485951597@qq.com>
2024-10-29 22:11:02 +0800

    Add Aimdirection to TS

--- Assets/Script/Player/Skill/ThrowSwordSkill.cs
+++ Assets/Script/Player/Skill/ThrowSwordSkill.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ThrowSwordSkill : Skill
 {
@@ -8,10 +9,22 @@ public class ThrowSwordSkill : Skill
     [SerializeField] Vector2 launchDir;
     [SerializeField] float swordGravity;
     [SerializeField] GameObject swordPrefab;
+    private Vector2 finalDir;
 
     public void CreateSword( )
     {
         GameObject newSword = Instantiate( swordPrefab, player.transform.position, transform.rotation);
         newSword.GetComponent<TS_Skill_Controller>().SetupSword(launchDir, swordGravity);
+    }
+
+    private Vector2 AimDirection()
+    {
+        Vector2 aimDirection = launchDir;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            aimDirection.y += 5;
+        }
+
+        return aimDirection;
     }
 }
