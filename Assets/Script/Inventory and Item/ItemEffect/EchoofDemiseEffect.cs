3f8432bab96c66e386b4bd7d4ad93610daa052fe
EEEeEFA <167228653+EEEeEFA@users.noreply.github.com>
2024-11-20 01:08:12 +0800

    Echo of Demise

---
+++ Assets/Script/Inventory and Item/ItemEffect/EchoofDemiseEffect.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Effect ", menuName = "Data/Item Effect/Echo of Demise")]
+public class EchoofDemiseEffect : itemEffect
+{
+    public override void UseEffect(Transform _enemyTarget)
+    {
+        base.UseEffect(_enemyTarget);
+    }
+    //TODO 回忆血条做法
+    //血量低于10%
+    //在DecreaseHealthBy(int _damage)里触发？
+
+    //减速周围敌人90% 2秒
+}
