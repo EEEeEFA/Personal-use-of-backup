bdd879e968a8deefaa7468f635193b6ad6bbb4d4
EEEeEFA <485951597@qq.com>
2024-12-03 21:47:17 +0800

    淡入淡出1

--- Assets/LoadedController.cs
+++ Assets/LoadedController.cs
@@ -9,6 +9,7 @@ public class UI_MainMenu : MonoBehaviour
 {
     [SerializeField] private string sceneName = "SampleScene";
     [SerializeField] private GameObject continueButton;
+    [SerializeField] UI_FadeScreen fadeScreen;//引用UI_FadeScreen脚本
 
     private void Start()
     {
@@ -19,17 +20,24 @@ public class UI_MainMenu : MonoBehaviour
 
     public void ContinueGame()
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadSceneWithFadeEffect(1.5f));
     }
 
     public void NewGame()
     {
         SaveManager.instance.DeleteSaveData();
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadSceneWithFadeEffect(1.5f));
     }
 
     public void ExitGame()
     {
         Debug.Log("退出游戏");
+    }
+
+    IEnumerator LoadSceneWithFadeEffect(float _delay)//加载场景的协程
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(_delay);
+        SceneManager.LoadScene(sceneName);
     }
 }
