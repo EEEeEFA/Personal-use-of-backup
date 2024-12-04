2326197c5d803408bef2204856d31c17b7bb7d83
EEEeEFA <167228653+EEEeEFA@users.noreply.github.com>
2024-12-05 05:20:14 +0800

    修复BUG：按Respawn没反应，因为是LoadScene

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -25,13 +25,13 @@ public class GameManager : MonoBehaviour,ISaveManager
     public void ReStart()//重新加载本场景
     {
         Scene scene = SceneManager.GetActiveScene();
-        SceneManager.SetActiveScene(scene);
+        SceneManager.LoadScene(scene.name);
         Debug.Log("ReStart");
     }
 
     public void LoadData(GameData _data)
     {
-        foreach (CheckPoint checkPoint in checkPoints)//通过checkpointId一一对应存档与场景中的checkpoint,并激活存档时被激活的checkpoint
+        foreach (CheckPoint checkPoint in checkPoints)//通过checkpointId一一对应存档与场景中的checkpoint,并激活被激活的checkpoint
         {
             foreach(KeyValuePair<string, bool>savedCheckPoint in _data.chekcpoints)
             {
@@ -40,6 +40,14 @@ public class GameManager : MonoBehaviour,ISaveManager
                     checkPoint.ActivateCheckpoint();
                 }
 
+            }
+        }
+
+        foreach(CheckPoint checkPoint in checkPoints)
+        {
+            if(checkPoint.checkpointId == _data.closestCheckPointID)
+            {
+                PlayerManager.instance.player.transform.position = checkPoint.transform.position;
             }
         }
     }
@@ -65,7 +73,7 @@ public class GameManager : MonoBehaviour,ISaveManager
         {
             if(point.activated == true)
             {
-                float distance = Vector2.Distance(PlayerManager.instance.player.transform.position, point.transform.position)
+                float distance = Vector2.Distance(PlayerManager.instance.player.transform.position, point.transform.position);
                 if (distance < closestDistance)
                 {
                     closestDistance = distance;
